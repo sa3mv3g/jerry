be75b2e6f92f0e64e0a6ea1a8565fe2a6b1e06df
Sanmveg Saini <sanmveg.saini.in@gamil.com>
2026-01-15 13:11:30 +0530

    build: modernize dependency management and integrate LwIP
    - Implement `FetchContent` for managing external dependencies (FreeRTOS, CMSIS, LwIP).
    - Configure dependencies to be downloaded to `application/dependencies/`.
    - Add `application/dependencies/lwip/CMakeLists.txt` to define LwIP targets.
    - Integrate LwIP into the main application build.
    - Update `.gitignore` to exclude fetched dependency directories.
    
    Signed-off-by:
    Sanmveg Saini
    <sanmveg.saini.in@gmail.com>

--- application/cmsis.cmake
+++ application/cmsis.cmake
@@ -1,5 +1,5 @@
 add_library(cmsis INTERFACE)
 
 target_include_directories(cmsis INTERFACE
-    ${CMAKE_CURRENT_LIST_DIR}/CMSIS_5/CMSIS/Core/Include
+    ${CMAKE_CURRENT_LIST_DIR}/dependencies/CMSIS_5/CMSIS/Core/Include
 )
